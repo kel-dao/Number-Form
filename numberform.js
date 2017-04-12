dbe741e58d91dfe753a44d1e9825bf3d62bc11c5
kel-dao <dao.kellya@yahoo.com>
2017-04-11 20:30:16 -0700

    add css js and html

---
+++ numberform.js
@@ -0,0 +1,25 @@
+var inputs = [];
+var sum = 0;
+var avg = 0;
+function addFunction(event) {
+  var myNumber = document.mathForm.myNumber.value;
+  var number = Number(myNumber);
+  if(!isNaN(number))  {
+    inputs.push(number);
+    sum += number;
+    avg = sum/inputs.length;
+    document.getElementById("count").innerHTML = inputs.length;
+    document.getElementById("sum").innerHTML = sum;
+    document.getElementById("avg").innerHTML = avg;
+    console.log(inputs);
+    console.log(sum);
+    console.log(avg);  
+  }
+  event.preventDefault();
+}
+
+function formReset(event) {
+    sum = 0;
+    avg = 0;
+    inputs = [];
+}
